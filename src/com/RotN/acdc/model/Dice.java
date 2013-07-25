f257f9bcd835694f686b9b785235c3466991f4c5
dvdvrhs <dave.vorhies@gmail.com>
2013-07-25 10:53:40 -0500

    Update Dice drawing so they don't interfere with the button

--- src/com/RotN/acdc/model/Dice.java
+++ src/com/RotN/acdc/model/Dice.java
@@ -19,9 +19,9 @@ public class Dice {
 	
 	public void draw(Canvas canvas, TheGameImpl game) {
 		
-		Bitmap die = diceBitmaps.get(getBlackResourceId(game.getB1DieValue()));
+		Bitmap die = diceBitmaps.get(getBlackResourceId(game.getB2DieValue()));
 		if (die != null) {
-			canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.2), (float)((canvas.getHeight() * 0.43) ), null);
+			canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.34375), (float)((canvas.getHeight() * 0.43) ), null);
 		} else {
 			die = diceBitmaps.get(getBlackResourceId(2));
 			//canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.035), (float)(canvas.getHeight() * 0.43), null);
@@ -34,9 +34,9 @@ public class Dice {
 			canvas.drawBitmap(die, null, dest, null);
 		}
 		
-		die = diceBitmaps.get(getBlackResourceId(game.getB2DieValue()));
+		die = diceBitmaps.get(getBlackResourceId(game.getB1DieValue()));
 		if (die != null) {
-			canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.3), (float)((canvas.getHeight() * 0.43) ), null);
+			canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.2578125), (float)((canvas.getHeight() * 0.43) ), null);
 			
 		} else {
 			die = diceBitmaps.get(getBlackResourceId(1));
@@ -52,7 +52,7 @@ public class Dice {
 		
 		die = diceBitmaps.get(getWhiteResourceId(game.getW1DieValue()));
 		if (die != null) {
-			canvas.drawBitmap(die, (float)(canvas.getWidth() * .65), (float)(canvas.getHeight() * 0.43), null);
+			canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.703125), (float)(canvas.getHeight() * 0.43), null);
 		} else {
 			die = diceBitmaps.get(getWhiteResourceId(2));
 			//canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.915), (float)(canvas.getHeight() * 0.43), null);
@@ -67,7 +67,7 @@ public class Dice {
 		
 		die = diceBitmaps.get(getWhiteResourceId(game.getW2DieValue()));
 		if (die != null) {
-			canvas.drawBitmap(die, (float)(canvas.getWidth() * .75), (float)(canvas.getHeight() * 0.43), null);
+			canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.79296875), (float)(canvas.getHeight() * 0.43), null);
 		} else {
 			die = diceBitmaps.get(getWhiteResourceId(1));
 			//canvas.drawBitmap(die, (float)(canvas.getWidth() * 0.915), (float)(canvas.getHeight() * 0.51), null);
